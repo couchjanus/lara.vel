04ef86878035612df9d3f5101e49e6017f7772ac
couch Janus <couchjanus@gmail.com>
2018-02-16 13:39:31 +0200

    Changed layout blog

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -6,51 +6,34 @@
 @section('title')
 @endsection
 
-      <!-- Page Heading/Breadcrumbs -->
-      <h1 class="mt-4 mb-3">{{ $post->title }}
-        <small>by
-          <a href="#">Janus Nic</a>
-        </small>
-      </h1>
+<!-- Page Heading/Breadcrumbs -->
+<h1 class="mt-4 mb-3">{{ $post->title }}
+  <small>by
+    <a href="#">Janus Nic</a>
+  </small>
+</h1>
 
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item">
-          <a href="/">Home</a>
-        </li>
-        <li class="breadcrumb-item">
-          <a href="{{ route('blog') }}">Blog</a>
-        </li>
-        <li class="breadcrumb-item active">{{ $post->title }}</li>
-      </ol>
+<ol class="breadcrumb">
+  <li class="breadcrumb-item">
+    <a href="/">Home</a>
+  </li>
+  <li class="breadcrumb-item">
+    <a href="{{ route('blog') }}">Blog</a>
+  </li>
+  <li class="breadcrumb-item active">{{ $post->title }}</li>
+</ol>
 
 
 @section('content')
 <!-- Post Content Column -->
 <div class="col-lg-8">
-
-    <!-- Preview Image -->
-    <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
-
+    @includeIf('blog.partials.single-post', ['post' => $post])
     <hr>
-
-    <!-- Date/Time -->
-    <p>Posted on {{ date('d F Y', strtotime($post->created_at)) }}</p>
-
-    <hr>
-
-    <!-- Post Content -->
-    <p class="lead">{{ $post->content }}</p>
-
-    <hr>
-    
     @includeIf('blog.partials.quote', ['some' => 'data'])
-    
     @includeWhen($hescomment, 'blog.partials.comments', ['some' => 'data'])
-
-  </div>
+</div>
 @endsection
 
 @section('sidebar')
   @include('blog.partials.sidebar')
 @endsection
-<!-- /.row -->
--- resources/views/shared/navigation.blade.php
+++ resources/views/shared/navigation.blade.php
@@ -8,13 +8,13 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href="about.html">About</a>
+              <a class="nav-link" href="/about">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="services.html">Services</a>
+              <a class="nav-link" href="/blog">Blog</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contact</a>
+              <a class="nav-link" href="/contact">Contact</a>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -26,16 +26,6 @@
                 <a class="dropdown-item" href="portfolio-3-col.html">3 Column Portfolio</a>
                 <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
                 <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a>
-              </div>
-            </li>
-            <li class="nav-item active dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Blog
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
-                <a class="dropdown-item active" href="blog-home-1.html">Blog Home 1</a>
-                <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
-                <a class="dropdown-item" href="blog-post.html">Blog Post</a>
               </div>
             </li>
             <li class="nav-item dropdown">
